f97d26684dca68fc1143172f3c0c14c9657688ab
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-21 15:38:51 +0200

    Init project

---
+++ XMessenger/XMessenger.Web/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Services
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#endregion
+
+var app = builder.Build();
+
+#region Config
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+#endregion
