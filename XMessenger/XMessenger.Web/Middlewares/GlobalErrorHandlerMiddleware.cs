aeb1a50006b98053e2698b49b951a9b4ea6fae2b
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-25 16:41:40 +0200

    Create global error handler

---
+++ XMessenger/XMessenger.Web/Middlewares/GlobalErrorHandlerMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+namespace XMessenger.Web.Middlewares
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public GlobalErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+                return;
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            var requestId = httpContext.TraceIdentifier;
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await httpContext.Response.WriteAsJsonAsync(new
+            {
+                Ok = false,
+                Error = "Internal server error",
+                Data = requestId
+            });
+        }
+    }
+
+    public static class GlobalErrorHandlerMiddlewareExtensions
+    {
+        public static void UseGlobalErrorHandler(this IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<GlobalErrorHandlerMiddleware>();
+        }
+    }
+}
--- XMessenger/XMessenger.Web/Program.cs
+++ XMessenger/XMessenger.Web/Program.cs
@@ -1,3 +1,5 @@
+using XMessenger.Web.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services
@@ -23,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseGlobalErrorHandler();
 
 app.UseApiVersioning();
 
