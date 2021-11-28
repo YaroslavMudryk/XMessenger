d2df33b06c65d7f946ba52fa1018944a6280aa0d
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-28 18:21:15 +0200

    Added ctors fro ApiResponse

--- XMessenger/XMessenger.Web/Responses/ApiResponse.cs
+++ XMessenger/XMessenger.Web/Responses/ApiResponse.cs
@@ -2,6 +2,25 @@
 {
     public class ApiResponse
     {
+        public ApiResponse() : this(true, null, null)
+        {
+
+        }
+        public ApiResponse(object data) : this(true, null, data)
+        {
+
+        }
+        public ApiResponse(string error, object data = null) : this(false, error, data)
+        {
+
+        }
+        public ApiResponse(bool ok, string error, object data)
+        {
+            Ok = ok;
+            Error = error;
+            Data = data;
+        }
+
         public bool Ok { get; set; }
         public string Error { get; set; }
         public object Data { get; set; }
