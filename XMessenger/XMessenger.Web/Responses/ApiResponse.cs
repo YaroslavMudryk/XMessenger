7f197a206a6874a473f86160eae9b85428490c73
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-28 18:13:25 +0200

    Added ApiResponse

---
+++ XMessenger/XMessenger.Web/Responses/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace XMessenger.Web.Responses
+{
+    public class ApiResponse
+    {
+        public bool Ok { get; set; }
+        public string Error { get; set; }
+        public object Data { get; set; }
+    }
+}
