07c3e836145a8883f5344279bc66eb76727a16e2
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-30 19:10:09 +0200

    US-13 (Create User controller)

---
+++ XMessenger/XMessenger.Web/Controllers/V1/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace XMessenger.Web.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    public class UsersController : ApiBaseController
+    {
+        [HttpGet("me")]
+        public IActionResult GetMe()
+        {
+            return Ok();
+        }
+    }
+}
