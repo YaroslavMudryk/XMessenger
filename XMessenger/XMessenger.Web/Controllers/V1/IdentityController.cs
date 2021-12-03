8c042b9d71bd3ca344ab00b482d0fa5bd04a2a1b
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-03 12:19:40 +0200

    Fix param in identity actions

--- XMessenger/XMessenger.Web/Controllers/V1/IdentityController.cs
+++ XMessenger/XMessenger.Web/Controllers/V1/IdentityController.cs
@@ -65,13 +65,13 @@ namespace XMessenger.Web.Controllers.V1
         }
 
         [HttpGet("sessions/{sessionId}")]
-        public IActionResult GetSessionById(long id)
+        public IActionResult GetSessionById(long sessionId)
         {
             return Ok();
         }
 
         [HttpDelete("sessions/{sessionId}")]
-        public IActionResult CloseSessionById(long id)
+        public IActionResult CloseSessionById(long sessionId)
         {
             return Ok();
         }
