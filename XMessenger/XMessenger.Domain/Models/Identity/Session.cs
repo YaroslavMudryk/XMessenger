44e1502c2596e14396c59e450567b1070b69c695
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2022-03-21 15:40:21 +0200

    Attributes were added to session model

--- XMessenger/XMessenger.Domain/Models/Identity/Session.cs
+++ XMessenger/XMessenger.Domain/Models/Identity/Session.cs
@@ -1,14 +1,21 @@
 ﻿using Extensions.DeviceDetector.Models;
+using System.ComponentModel.DataAnnotations;
+
 namespace XMessenger.Domain.Models.Identity
 {
     public class Session : BaseModel<Guid>
     {
+        [Required]
         public App App { get; set; }
+        [Required]
         public Location Location { get; set; }
+        [Required]
         public ClientInfo Client { get; set; }
+        [Required]
         public bool IsActive { get; set; }
         public DateTime? DeactivatedAt { get; set; }
         public Guid? DeactivatedBySessionId { get; set; }
+        [Required, StringLength(50, MinimumLength = 40)]
         public string RefreshToken { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
