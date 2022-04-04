c2897578e22505d3482511f0edaa04cae49f4ba1
Nikita Medko <nikita.jove28@gmail.com>
2022-04-04 13:53:37 +0300

    Models created in DB and added to DbContext

---
+++ XMessenger/XMessenger.Domain/Models/Identity/Claim.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XMessenger.Domain.Models.Identity
+{
+    public class Claim : BaseModel<int>
+    {
+        [Required, StringLength(500)]
+        public string Type { get; set; }
+        [Required, StringLength(500)]
+        public string Value { get; set; }
+        [StringLength(500, MinimumLength = 1)]
+        public string DisplayText { get; set; }
+        public List<RoleClaim> RoleClaims { get; set; }
+        //public List<AppClaim> AppClaims{ get; set; } //reference for the future AppClaims
+    }
+}
