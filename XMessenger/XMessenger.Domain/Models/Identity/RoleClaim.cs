19e6d101bf4268fb56d91ae64576af195d7d7a29
Nikita Medko <nikita.jove28@gmail.com>
2022-04-04 12:34:28 +0300

    Created models to db and added to DbContext

---
+++ XMessenger/XMessenger.Domain/Models/Identity/RoleClaim.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMessenger.Domain.Models.Identity
+{
+    public class RoleClaim : BaseModel<int>
+    {
+        public int ClaimId { get; set; }
+        public Claim Claim { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
