e3c60d5860a4f2e259eb79324393d14bab6f2b65
MykhailoMelnyk <43968821+msmelnyk@users.noreply.github.com>
2022-04-17 00:09:44 +0300

    XM-28: completed

---
+++ XMessenger/XMessenger.Domain/Models/Identity/LoginAttempt.cs
@@ -0,0 +1,13 @@
+﻿
+using Extensions.DeviceDetector.Models;
+
+namespace XMessenger.Domain.Models.Identity;
+
+public class LoginAttempt: BaseModel<int>
+{
+    public string Password { get; set; }
+    public ClientInfo Device { get; set; }
+    public Location Location { get; set; }
+    public bool IsSuccess { get; set; }
+    public int? UserId { get; set; }
+}
